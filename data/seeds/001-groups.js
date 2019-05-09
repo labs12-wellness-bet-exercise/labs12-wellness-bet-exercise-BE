a2534c0a5308bcdbd5b32032ac56801cbbaa241f
Arkadiusz Kusiak <akusiak23@gmail.com>
2019-05-09 15:45:42 -0500

    Admin boolean changes

--- data/seeds/001-groups.js
+++ data/seeds/001-groups.js
@@ -11,9 +11,11 @@ exports.seed = function(knex, Promise) {
           start_date: "Jan 1, 2020",
           end_date: "Jan 30, 2020",
           join_code: "BUGS",
-          group_message: "Welcome to Group 1", 
-          group_photo: "https://images2.minutemediacdn.com/image/upload/c_fill,w_912,h_516,f_auto,q_auto,g_auto/shape/cover/entertainment/5b2fefb9fae96f45b7000002.jpeg",
-          pot_total: 1000.0
+          group_message: "Welcome to Group 1",
+          group_photo:
+            "https://images2.minutemediacdn.com/image/upload/c_fill,w_912,h_516,f_auto,q_auto,g_auto/shape/cover/entertainment/5b2fefb9fae96f45b7000002.jpeg",
+          pot_total: 1000.0,
+          admin_id: 1
         }),
         knex("groups").insert({
           group_name: "Group 2",
@@ -23,7 +25,8 @@ exports.seed = function(knex, Promise) {
           join_code: "BUGS2",
           group_message: "Welcome to Group 2",
           group_photo: "https://fiftiesweb.com/tv/mod-squad-c.jpg",
-          pot_total: 2000.0
+          pot_total: 2000.0,
+          admin_id: 2
         }),
         knex("groups").insert({
           group_name: "Group 3",
@@ -32,8 +35,10 @@ exports.seed = function(knex, Promise) {
           end_date: "Mar 30, 2020",
           join_code: "BUGS3",
           group_message: "Welcome to Group 3",
-          group_photo: "https://img.huffingtonpost.com/asset/5a4ef0931c0000f98768eb07.jpg?ops=scalefit_720_noupscale",
-          pot_total: 3000.0
+          group_photo:
+            "https://img.huffingtonpost.com/asset/5a4ef0931c0000f98768eb07.jpg?ops=scalefit_720_noupscale",
+          pot_total: 3000.0,
+          admin_id: 3
         })
       ]);
     });
--- middleware/routes/groupsRouter.js
+++ middleware/routes/groupsRouter.js
@@ -1,117 +1,207 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../../helpers/groupHelpers');
+const db = require("../../helpers/groupHelpers");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   db.find()
-    .then( groups => {
-      res
-        .status(200)
-        .json(groups);
+    .then(groups => {
+      res.status(200).json(groups);
     })
     .catch(err => {
-      res
-        .status(500)
-        .json({
-          error: "Something went wrong. We can't show you the list of groups.",
-          err
-        })
-    })
-})
+      res.status(500).json({
+        error: "Something went wrong. We can't show you the list of groups.",
+        err
+      });
+    });
+});
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const id = req.params.id;
   db.findById(id)
     .then(group => {
-      res
-        .status(200)
-        .json(group);
+      if (group.length > 0) {
+        res.status(200).json(group);
+      } else {
+        res.status(404).json({ error: "Couldn't find a group with that ID." });
+      }
     })
     .catch(err => {
-      res 
-        .status(500)
-        .json({ error: "Problem finding that group...", err })
-    })
+      res.status(500).json({ error: "Problem finding that group...", err });
+    });
 });
 
-router.get('/:id/participants', (req, res) => {
+router.post("/", async (req, res) => {
+  const group = req.body;
+  !group.group_name
+    ? res.status(406).json({
+        error: "Your group needs a name."
+      })
+    : group.buy_in_amount < 0
+    ? res.status(406).json({
+        error:
+          "Your buy-in amount cannot be a negative number. How would that even work?"
+      })
+    : !(group.start_date && group.end_date)
+    ? res.status(406).json({
+        error: "Your competition must have a beginning and end date."
+      })
+    : !group.group_message
+    ? res.status(406).json({
+        error:
+          "Please enter a group message, even if it's just one letter. But why not make it more than one letter? Take the time to send a warm welcome to your group. Or don't. I'm fine either way. But like I said before, it has to be at least one letter. Them's the rules."
+      })
+    : createGroup();
+
+  async function createGroup() {
+    try {
+      const groupId = await db.insert(group);
+      console.log(groupId);
+      const newGroup = await db.findById(groupId.id);
+      console.log(newGroup);
+      res.status(201).json({
+        message: "Congratulations. Your group was successfully added.",
+        newGroup
+      });
+    } catch (error) {
+      res.status(500).json({
+        errorMessage: "There was some sort of problem registering your group",
+        error
+      });
+    }
+  }
+});
+
+router.get("/:id/participants", (req, res) => {
   const id = req.params.id;
   db.getGroupParticipants(id)
     .then(participants => {
       if (participants.length == 0) {
-        res
-          .status(404)
-          .json({ message: "That group ID number isn't associated with any participants. \nAre you sure that group exists?"})
+        res.status(404).json({
+          message:
+            "That group ID number isn't associated with any participants. \nAre you sure that group exists?"
+        });
       }
-        res
-          .status(200)
-          .json(actions);
+      res.status(200).json(actions);
     })
     .catch(error => {
-      res 
-        .status(500)
-        .json({ 
-          error: "Error finding participants associated with that group ID...",
-          error
-      })
-    })
-})
+      res.status(500).json({
+        error: "Error finding participants associated with that group ID...",
+        error
+      });
+    });
+});
 
 // GROUP PHOTOS -- photo link for now
 
 // get group photo
-router.get('/groupphoto/:id', (req, res) => {
+router.get("/groupphoto/:id", (req, res) => {
   db.getGroupPhoto(req.params.id)
-  .then(group_photo => {
-    res.status(200)
-    .json(group_photo)
-  })
-  .catch(error => {
-    res.status(500)
-    .json({error: 'There was an error getting your group photo', error})
-  })
+    .then(group_photo => {
+      res.status(200).json(group_photo);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "There was an error getting your group photo", error });
+    });
 });
 
-// change group photo 
+// change group photo
 
-router.put('/groupphoto/:id', (req, res) => {
+router.put("/groupphoto/:id", (req, res) => {
   db.addGroupPhoto(req.params.id, req.body.group_photo)
-  .then(() => {
-    res.status(200).json({message: 'photo successfully changed'})
-  })
-  .catch(error => {
-    res.status(500)
-    .json(error)
-  })
-}) 
+    .then(() => {
+      res.status(200).json({ message: "photo successfully changed" });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
-// change group photo back to default 
+// change group photo back to default
 
-router.put('/groupphoto/:id/default', (req, res) => {
+router.put("/groupphoto/:id/default", (req, res) => {
   db.toDefaultGroupPhoto(req.params.id)
-  .then(() => {
-    res.status(200)
-    .json({message: 'group photo changed back to default'})
-  })
-  .catch(error => {
-    res.status(500)
-    .json(error)
-  })
-})
-
+    .then(() => {
+      res.status(200).json({ message: "group photo changed back to default" });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 // delete group photo -- needs group id  -- You can't actually use a true delete on this or it will delete the whole group because of how the tables are structured, so I did it as an PUT request and made it so it would update the photo to "group photo does not exist"
 
-router.put('/groupphoto/:id/delete', (req, res) => {
+router.put("/groupphoto/:id/delete", (req, res) => {
   db.deleteGroupPhoto(req.params.id)
-  .then( deleted => {
-    res.status(200)
-    .json({message: 'photo deleted', deleted}) 
-  })
-  .catch(error => {
-    res.status(500)
-    .json(error)
-  })
-})
+    .then(deleted => {
+      res.status(200).json({ message: "photo deleted", deleted });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+//get admin message
+router.get("/adminmessage/:id", (req, res) => {
+  db.getAdminMessage(req.params.id)
+    .then(group_message => {
+      res.status(200).json(group_message);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error getting your admin message",
+        error
+      });
+    });
+});
+
+//update admin message
+router.put("/adminmessage/:id", (req, res) => {
+  db.updateAdminMessage(req.params.id, req.body.group_message)
+    .then(() => {
+      res.status(200).json({ message: "admin message successfully changed" });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+//change admin message to default
+
+router.put("/adminmessage/:id/default", (req, res) => {
+  db.toDefaultAdminMessage(req.params.id)
+    .then(() => {
+      res
+        .status(200)
+        .json({ message: "admin message changed back to default" });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+//change admin message to admin message does not exist
+router.put("/adminmessage/:id/delete", (req, res) => {
+  db.deleteAdminMessage(req.params.id)
+    .then(deleted => {
+      res.status(200).json({ message: "admin message deleted", deleted });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+// GET GROUP JOIN CODE
+
+router.get("/:id/join_code", (req, res) => {
+  db.getJoinCode(req.params.id)
+    .then(join_code => {
+      res.status(200).json(join_code);
+    })
+    .catch(error => {
+      res.status(500).json({ message: `There was an error.`, error: error });
+    });
+});
 
 module.exports = router;
