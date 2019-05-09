abe628bb41e94a47816b653b40a782ab2c073bd4
Joe Bugajski <yogurtmystery@gmail.com>
2019-05-09 17:27:49 -0500

    adjusted group participants migrations

--- data/migrations/20190502153324_group_participants.js
+++ data/migrations/20190502153324_group_participants.js
@@ -28,8 +28,9 @@ exports.up = function(knex, Promise) {
       .defaultTo(false)
       .notNullable();
 
-    table.string("buyin_proof"), defaultTo(null);
-
+    table
+      .string("buyin_proof")
+      .defaultTo('');
   });
 };
 
