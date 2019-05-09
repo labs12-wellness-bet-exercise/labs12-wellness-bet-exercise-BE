2996d0bdec1978d1be5a14cac2e1d8bc24c278f3
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-05-09 16:17:38 -0500

    added endpoint to readme and endpoint to participants router

--- middleware/routes/participantsRouter.js
+++ middleware/routes/participantsRouter.js
@@ -1,37 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../../helpers/participantsHelpers');
+const db = require("../../helpers/participantsHelpers");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   db.find()
-    .then( participants => {
-      res
-        .status(200)
-        .json(participants);
+    .then(participants => {
+      res.status(200).json(participants);
     })
     .catch(err => {
-      res
-        .status(500)
-        .json({
-          error: "Sorry, we had a bit of trouble finding the list of participants.",
-          err
-        })
-    })
-})
+      res.status(500).json({
+        error:
+          "Sorry, we had a bit of trouble finding the list of participants.",
+        err
+      });
+    });
+});
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const id = req.params.id;
   db.findById(id)
     .then(participant => {
-      res
-        .status(200)
-        .json(participant);
+      res.status(200).json(participant);
     })
     .catch(err => {
-      res 
+      res
         .status(500)
-        .json({ error: "Problem finding that participant...", err })
-    })
+        .json({ error: "Problem finding that participant...", err });
+    });
+});
+
+router.post("/", async (req, res) => {
+  db.insert(req.body)
+    .then(group_participant => res.status(200).json(group_participant))
+    .catch(error => {
+      res.status(500).json({ message: "There was an error", error });
+    });
 });
 
 module.exports = router;
