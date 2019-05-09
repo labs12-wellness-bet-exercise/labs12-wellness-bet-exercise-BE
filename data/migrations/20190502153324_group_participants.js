e6635d404a6366d7ba4ed8f773c8c065911ea322
Joe Bugajski <yogurtmystery@gmail.com>
2019-05-09 16:27:18 -0500

    fixed typo in group participants migration

--- data/migrations/20190502153324_group_participants.js
+++ data/migrations/20190502153324_group_participants.js
@@ -21,7 +21,7 @@ exports.up = function(knex, Promise) {
       .defaultTo(false)
       .notNullable();
 
-    table.string("buyin_proof"), defaultTo(null);
+    table.string("buyin_proof").defaultTo('');
   });
 };
 
