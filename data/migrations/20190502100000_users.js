42e7ff766fa314e88697b7c6db1a094aae0c5df3
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-05-13 18:19:57 -0500

    converted first_name and last_name into display_name in users table migration and in seeds

--- data/migrations/20190502100000_users.js
+++ data/migrations/20190502100000_users.js
@@ -2,9 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("users", table => {
     table.increments("user_id");
 
-    table.string("first_name").notNullable();
-
-    table.string("last_name").notNullable();
+    table.string("display_name").notNullable();
 
     table.string("google_uuid").unique();
 
--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -24,22 +24,19 @@ exports.seed = async function(knex, Promise) {
       // Inserts seed entries
       return knex("users").insert([
         {
-          first_name: "Jane",
-          last_name: "Fonda",
+          display_name: "Jane Fonda",
           email: "janefonda4lyfe@gmail.com",
           profilePhoto:
             "https://pixel.nymag.com/imgs/fashion/daily/2015/02/25/25-jane-fonda-lede.w330.h412.jpg"
         },
         {
-          first_name: "Richard",
-          last_name: "Simmons",
+          display_name: "Richard Simmons",
           email: "richardnotgene@gmail.com",
           profilePhoto:
             "https://cf-images.us-east-1.prod.boltdns.net/v1/static/769341148/b973d4fb-8d82-4df8-a15a-4be17765b4db/036ecf3b-ed34-439f-bfb6-5dbe7676965d/1280x720/match/image.jpg"
         },
         {
-          first_name: "Jack",
-          last_name: "LaLanne",
+          display_name: "Jack LaLanne",
           email: "jacklalanne@aol.com",
           profilePhoto:
             "https://cbsnews2.cbsistatic.com/hub/i/r/2011/01/24/bd3d463d-a642-11e2-a3f0-029118418759/resize/620x465/0d0d9383885c4882b821593ff0507a2f/AP80022001195.jpg"
