1725b325aee48c4c99d2f5725f6c0751e3fcc56a
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-05-16 22:40:49 -0500

    created getUsersByMembership endpoint

--- helpers/userGroupsHelpers.js
+++ helpers/userGroupsHelpers.js
@@ -1,7 +1,8 @@
 const db = require("../data/dbConfig.js");
 
 module.exports = {
-  getGroupsByUser
+  getGroupsByUser,
+  getUsersByMembership
 };
 
 function getGroupsByUser(id) {
@@ -10,4 +11,12 @@ function getGroupsByUser(id) {
     .from("group_participants")
     .leftJoin("groups", "group_participants.group_id", "groups.group_id")
     .where({ user_id: Number(id) });
+}
+
+function getUsersByMembership(group_id) {
+  return db
+    .select("*")
+    .from("group_participants")
+    .leftJoin("users", "group_participants.user_id", "users.user_id")
+    .where({ group_id: Number(group_id) });
 }
